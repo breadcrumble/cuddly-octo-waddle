2fd4e1c51f62c9e13ec8ffc52c57d867c49ea3a4
Hui Suan <chunghuisuan@gmail.com>
2015-09-05 17:22:55 +0800

    Finally got chartJS to work

--- app.js
+++ app.js
@@ -39,7 +39,12 @@ angular.module('app', ['ui.bootstrap', 'chart.js', 'ui.router'])
     return window._;
   })
   .controller('WATenController', function($scope, $http) {
-
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
 
 
     //calendar
